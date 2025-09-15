234076c6162a6fb14acb12d4b992706a9a73688d
KCW23 <kcw051219@gmail.com>
2025-09-15 16:25:43 +0900

    현장연습문제_#0

--- 현장연습문제_#0/현장연습문제_#0.cpp
+++ 현장연습문제_#0/현장연습문제_#0.cpp
@@ -3,11 +3,11 @@
 int main() {
 
     printf("=== Basic Constants === \n\n");
-    printf("Name    : Choonwoo \n\n");
-    printf("Initial : C \n\n");
+    printf("Name\t: Choonwoo \n\n");
+    printf("Initial\t: C \n\n");
     printf("Age : 21 \n\n");
-    printf("Score   :93.500000 \n\n");
-    printf("Count   :3000000000 \n\n");
+    printf("Score\t: 93.500000 \n\n");
+    printf("Count\t: 3000000000 \n\n");
 
     return 0;
 }
