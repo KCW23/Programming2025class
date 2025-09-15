f99d2b4402370f8a86080da04d51b19a0cadf381
LeeJeongBin0428 <jengbin@naver.com>
2025-09-15 16:09:17 +0900

    현장연습문제_#0
    현장연습문제_#0

---
+++ 현장연습문제_#0/현장연습문제_#0.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("=== Basic Constants ===\n");
+    printf("Name    : Choonwoo \n");
+    printf("Initial : C \n");
+    printf("Age : 21 \n");
+    printf("Score   :93.500000 \n");
+    printf("Count   :3000000000 \n");
+
+    return 0;
+}
